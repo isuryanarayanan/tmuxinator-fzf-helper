650622ba4d24a6d20de1a6a6c5bcd11ac65fedb6
isuryanarayanan <devdoodan@gmail.com>
2021-05-19 11:03:11 +0530

    editing tmuxinator project mode

--- tmuxinator-helper.sh
+++ tmuxinator-helper.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+
 # Checking FZF installation
 if [ $(dpkg-query -W -f='${Status}' fzf 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-	SUDO=''
 	# Requesting root
+
+	SUDO=''
 	if (( $EUID != 0 )); then
 			echo "Please run as root"
 			SUDO='sudo'
@@ -13,20 +15,31 @@ then
 	$SUDO apt-get install fzf;
 fi
 
-# Getting tmuxinator projects and active sessions
-SESSION=$( ((tmuxinator list -n| awk '{print substr($1, 0, length($1)-0) " (open)"}')  && tmux ls | grep : | cut -d. -f1 | awk '{print substr($1, 0, length($1)-1) " (close)"}') |
-	tail -n +2 |
-	fzf --border --prompt="Project: " -m -1 -q "$1")
+# In edit mode
+if [[ "$1" == "--edit" ]]; then
+	# Getting tmuxinator projects 
 
-# Session closer
-if echo "$SESSION" | grep '(close)'; then
-	CLOSESESSION=(${SESSION// / })
-	tmuxinator stop ${CLOSESESSION[0]};
+	SESSION=$(tmuxinator list -n | tail -n +2 | fzf --border --prompt="Project: " -m -1 -q "$2")
+	export EDITOR=vim && tmuxinator edit $SESSION
+
+# In standard mode
+else
+	# Getting tmuxinator projects and active sessions
+	
+	SESSION=$( ((tmuxinator list -n| awk '{print substr($1, 0, length($1)-0) " (open)"}')  && tmux ls | grep : | cut -d. -f1 | awk '{print substr($1, 0, length($1)-1) " (close)"}') |
+		tail -n +2 |
+		fzf --border --prompt="Project: " -m -1 -q "$1")
+
+	# Session closer
+	if echo "$SESSION" | grep '(close)'; then
+		CLOSESESSION=(${SESSION// / })
+		tmuxinator stop ${CLOSESESSION[0]};
+	fi
+
+	# Session starter
+	if echo "$SESSION" | grep '(open)'; then
+		CLOSESESSION=(${SESSION// / })
+		tmuxinator start ${CLOSESESSION[0]};
+	fi
+
 fi
-
-# Session starter
-if echo "$SESSION" | grep '(open)'; then
-	CLOSESESSION=(${SESSION// / })
-	tmuxinator start ${CLOSESESSION[0]};
-fi
-
